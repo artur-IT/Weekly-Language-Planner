d644e8f0aa95cf92e629f8bddee492d4e179e7c6
Artur Matysiak <inz.matys@gmail.com>
2024-01-29 15:14:42 +0100

    add localstorage and class TaskBox

--- main.js
+++ main.js
@@ -1,47 +1,54 @@
-// let newTask = {
-//   day: "",
-//   study: "",
-//   name: "",
-//   time: 0,
-// };
+// (init = () => {
+// })();
 
-let taskNames = [];
 let taskDB = [];
-const days = ["MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY", "SUNDAY"];
-const habits = ["SPEAKING", "READING", "WRITING", "LISTENING", "VOCABULARY"];
+// const days = ["MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY", "SUNDAY"];
+// const habits = ["SPEAKING", "READING", "WRITING", "LISTENING", "VOCABULARY"];
 
 getTaskFromUser = (e) => {
-  e.preventDefault();
   const myDay = document.querySelector(".day_task");
   const myStudy = document.querySelector(".study");
   const myTask = document.querySelector(".task_name");
   const myTime = document.querySelector(".task_time");
-  console.log(myDay.value, myStudy.value, myTask.value, myTime.value);
+  const id = myDay.value + "-" + myStudy.value;
+
+  e.preventDefault();
+  const newTask = new TaskBox(id, myDay.value, myStudy.value, myTask.value, myTime.value);
+
+  window.localStorage.setItem(id, JSON.stringify(newTask));
+  // console.log(newTask);
+  // taskDB.push(newTask);
+  for (let i = 0; i < window.localStorage.length; i++) {
+    console.log(window.localStorage);
+  }
 };
 
 document.querySelector("button").onclick = getTaskFromUser;
 
-for (let x = 0; x < days.length; x++) {
-  for (let y = 0; y < habits.length; y++) {
-    const newTaskName = days[x] + `-` + habits[y].toLocaleLowerCase();
-    const test = {
-      [newTaskName]: {
-        name: "",
-        time: 0,
-      },
-    };
-    taskDB.push(test);
-  }
-}
+// for (let x = 0; x < days.length; x++) {
+//   for (let y = 0; y < habits.length; y++) {
+//     const newTaskName = days[x].toLocaleLowerCase() + `-` + habits[y];
+//     //
+//     const test = {
+//       [newTaskName]: {
+//         name: "",
+//         time: 0,
+//       },
+//     };
+//     taskDB.push(test);
+//   }
+// }
 
-console.log(taskDB);
+// console.log(taskDB);
 
-class EmptyTaskBox {
-  constructor(day, study) {
-    this.day;
-    this.study;
-    name: "";
-    time: 0;
+class TaskBox {
+  constructor(id, day, study, name, time) {
+    this.id = id;
+    this.day = day;
+    this.study = study;
+    this.name = name;
+    this.time = time;
+    this.active = false;
   }
 
   addTask() {
