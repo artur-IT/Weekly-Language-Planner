89558651a77f635ee59e9746b08cf7fed7678471
Artur Matysiak <inz.matys@gmail.com>
2024-02-05 15:49:06 +0100

    add ShowTask React Component

--- main.js
+++ main.js
@@ -1,5 +1,50 @@
 // import GetTaskBox from "./GetTaskBox.jsx";
 
+// KOMPONENT wyświetla zadanie z LocalStorage
+const ShowTask = ({ taskValues }) => {
+  return (
+    <div>
+      <p>{taskValues.name}</p>
+      <p>{taskValues.time} min.</p>
+      <p>{taskValues.date_add}</p>
+    </div>
+  );
+};
+
+// search empty boxes and compare to LocalStore
+const GetTaskBox = () => {
+  const emptyBoxes = document.querySelectorAll(".empty");
+
+  for (const el of emptyBoxes) {
+    // get box name from DOM
+    const emptyBoxName = el.attributes.name.nodeValue;
+    const localStoreId = JSON.parse(localStorage.getItem(emptyBoxName));
+
+    // show all task's from LocalStore to empty boxes
+    if (localStoreId) {
+      el.style.backgroundColor = "yellow";
+      const empty = ReactDOM.createRoot(el);
+      empty.render(<ShowTask taskValues={localStoreId} />);
+    }
+  }
+};
+
+GetTaskBox();
+
+// object to new task from user
+class TaskBox {
+  constructor(id, date_add, day, study, name, time) {
+    this.id = id;
+    this.date_add = date_add;
+    this.day = day;
+    this.study = study;
+    this.name = name;
+    this.time = time;
+    // this.active = false;
+  }
+}
+
+// get new task from user and save to LocalStorage
 const getTaskFromUser = (e) => {
   e.preventDefault();
   const myDay = document.querySelector(".day_task");
@@ -16,67 +61,3 @@ const getTaskFromUser = (e) => {
 };
 
 document.querySelector("button").onclick = getTaskFromUser;
-
-// REACT COMPONENT
-const GetTaskBox = () => {
-  const emptyBoxes = document.querySelectorAll(".empty");
-
-  // for (let i = 0; i < localStorage.length; i++) {
-  // console.log(localStorage.key(i));
-
-  for (const el of emptyBoxes) {
-    const emptyBoxName = el.attributes.name.nodeValue;
-    const localStoreId = JSON.parse(localStorage.getItem(emptyBoxName));
-    console.log(localStoreId);
-
-    // if (emptyBoxName === localStoreId) {
-    //   el.style.backgroundColor = "yellow";
-    //   el.textContent = "oko";
-    //   console.log("jest");
-    // }
-  }
-
-  // }
-};
-//------------
-
-// for (let x = 0; x < days.length; x++) {
-//   for (let y = 0; y < habits.length; y++) {
-//     const newTaskName = days[x].toLocaleLowerCase() + `-` + habits[y];
-//     const test = {
-//       [newTaskName]: {
-//         name: "",
-//         time: 0,
-//       },
-//     };
-//     taskDB.push(test);
-//     console.log(newTaskName);
-//   }
-// }
-
-class TaskBox {
-  constructor(id, date_add, day, study, name, time) {
-    this.id = id;
-    this.date_add = date_add;
-    this.day = day;
-    this.study = study;
-    this.name = name;
-    this.time = time;
-    // this.active = false;
-  }
-
-  addTask() {
-    console.log(this.day, this.study);
-  }
-}
-
-GetTaskBox();
-
-const MyApp = () => {
-  return;
-  // <GetTaskBox />;
-  // <h1>Hello, world!</h1>;
-  // <GetTaskBox />;
-};
-const root = ReactDOM.createRoot(document.getElementById("app"));
-root.render(<MyApp />);
