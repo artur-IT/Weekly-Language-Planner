1bdc4a419e299d5bfffc1b7f8439bb34eb956547
Artur Matysiak <inz.matys@gmail.com>
2024-02-27 14:11:11 +0100

    add animation to sectinAddTask

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,16 +5,26 @@ const Header = () => {
 
   const showTaskBar = () => {
     const addBar = document.querySelector(".task_fields");
-    addBar.classList.toggle("show_task_bar");
+    // const tasksGrid = document.querySelector(".layout");
+
+    if (addBar.classList.contains("show_task_bar")) {
+      addBar.classList.remove("show_task_bar");
+      addBar.classList.add("hide_task_bar");
+    } else if (addBar.classList.contains("hide_task_bar")) {
+      addBar.classList.remove("hide_task_bar");
+      addBar.classList.toggle("show_task_bar");
+    }
   };
 
   return (
     <>
       <h2>Weekly Language Planner</h2>
-      <button className="add_top" onClick={showTaskBar} ref={myRef}>
-        Add Task
-      </button>
-      <button className="remove_top">Clear All</button>
+      <div className="two-buttons-top">
+        <button className="add_top" onClick={showTaskBar} ref={myRef}>
+          Add Task
+        </button>
+        <button className="remove_top">Clear All</button>
+      </div>
     </>
   );
 };
