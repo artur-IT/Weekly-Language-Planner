d044b7f8b48f46f11440298e26394256afc9c908
Artur Matysiak <inz.matys@gmail.com>
2024-02-27 19:41:24 +0100

    fix styles in css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,22 +9,24 @@ const Header = () => {
 
     if (addBar.classList.contains("show_task_bar")) {
       addBar.classList.remove("show_task_bar");
-      addBar.classList.add("hide_task_bar");
+      addBar.classList.toggle("hide_task_bar");
     } else if (addBar.classList.contains("hide_task_bar")) {
       addBar.classList.remove("hide_task_bar");
       addBar.classList.toggle("show_task_bar");
     }
+    addBar.classList.toggle("show_task_bar");
   };
 
   return (
     <>
-      <h2>Weekly Language Planner</h2>
-      <div className="two-buttons-top">
-        <button className="add_top" onClick={showTaskBar} ref={myRef}>
-          Add Task
-        </button>
-        <button className="remove_top">Clear All</button>
-      </div>
+      <h1>
+        Weekly Language Planner <img src="./src/icon/question.svg" alt="question symbol" />
+      </h1>
+
+      <button className="add_top" onClick={showTaskBar} ref={myRef}>
+        Add Task
+      </button>
+      <button className="remove_top">Clear All</button>
     </>
   );
 };
--- src/components/SectionAddTask.jsx
+++ src/components/SectionAddTask.jsx
@@ -1,40 +1,42 @@
 const AddTaskSection = () => {
   return (
-    <form className="task_fields" id="task_inputs">
-      <label>
-        Day:
-        <select className="day_task" name="day-task">
-          <option></option>
-          <option>Monday</option>
-          <option>Tuesday</option>
-          <option>Wednesday</option>
-          <option>Thursday</option>
-          <option>Friday</option>
-          <option>Saturday</option>
-          <option>Sunday</option>
-        </select>
-      </label>
-      <label>
-        Study:
-        <select className="study" name="study-name">
-          <option></option>
-          <option>SPEAKING</option>
-          <option>READING</option>
-          <option>WRITING</option>
-          <option>LISTENING</option>
-          <option>VOCABULARY</option>
-        </select>
-      </label>
-      <label>
-        Task name:
-        <input className="task_name" name="task-name" type="text" placeholder="task name..." />
-      </label>
-      <label>
-        Task time:
-        <input className="task_time" type="number" id="exercise_time" step="5" min="0" max="60" placeholder="max. 60" /> min.
-      </label>
-      <button className="add_form">Dodaj</button>
-    </form>
+    <div className="form-container">
+      <form className="task_fields" id="task_inputs">
+        <label>
+          Day:
+          <select className="day_task" name="day-task">
+            <option></option>
+            <option>Monday</option>
+            <option>Tuesday</option>
+            <option>Wednesday</option>
+            <option>Thursday</option>
+            <option>Friday</option>
+            <option>Saturday</option>
+            <option>Sunday</option>
+          </select>
+        </label>
+        <label>
+          Study:
+          <select className="study" name="study-name">
+            <option></option>
+            <option>SPEAKING</option>
+            <option>READING</option>
+            <option>WRITING</option>
+            <option>LISTENING</option>
+            <option>VOCABULARY</option>
+          </select>
+        </label>
+        <label>
+          Task name:
+          <input className="task_name" name="task-name" type="text" placeholder="task name..." />
+        </label>
+        <label>
+          Task time:
+          <input className="task_time" type="number" id="exercise_time" step="5" min="0" max="60" placeholder="max. 60" /> min.
+        </label>
+        <button className="add_form">Dodaj</button>
+      </form>
+    </div>
   );
 };
 
