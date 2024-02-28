af84eccd0802c6f5d56a04efb0394f4b1b614e45
Artur Matysiak <inz.matys@gmail.com>
2024-02-28 13:48:04 +0100

    fix position and content tooltip

--- src/components/Language.jsx
+++ src/components/Language.jsx
@@ -4,7 +4,7 @@ const Language = () => {
       <div className="languages">
         <div className="tooltip">
           <img className="" src="./src/icon/question.svg" alt="question symbol" />
-          <p className="tooltiptext">testy moje...</p>
+          <p className="tooltiptext">Zaplanuj naukę języka obcego, którego chcesz się nauczyć.</p>
         </div>
         <img src="./src/icon/pl.png" alt="polish language" />
         <img src="./src/icon/en.png" alt="english language" />
