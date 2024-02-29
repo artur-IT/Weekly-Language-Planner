be92c53eecc42c0c782438661c5fe7f12c6868c7
Artur Matysiak <inz.matys@gmail.com>
2024-02-29 10:00:37 +0100

    fix content tooltip

--- src/components/Language.jsx
+++ src/components/Language.jsx
@@ -19,7 +19,10 @@ const Language = (props) => {
       <div className="languages">
         <div className="tooltip">
           <img className="" src="./src/icon/question.svg" alt="question symbol" />
-          <p className="tooltiptext">Zaplanuj naukę języka obcego, którego chcesz się nauczyć.</p>
+          <p className="tooltiptext">
+            Accurate Plan to learn your foreign language. <br />
+            Type in a brief statement of what you will be doing, e.g., listening to the radio, reading an article, fiche, ...
+          </p>
         </div>
         <img src="./src/icon/pl.png" ref={plRef} onClick={langButton} alt="polish language" />
         <img src="./src/icon/en.png" ref={enRef} onClick={langButton} alt="english language" />
